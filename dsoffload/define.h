d209ad4e824032fa23dede688b51e286710a724d
Chair <yichih17@gmail.com>
2017-01-29 21:47:48 +0800

    UE can find a BS to connect

--- dsoffload/define.h
+++ dsoffload/define.h
@@ -2,41 +2,55 @@
 #define _DEFIEN_H
 
 #define UE_number 500	//UE個數
-#define AP_number 500	//AP個數
+#define AP_number 1000	//AP個數
 #define R 1723			//Macro eNB半徑
 #define Macro_power 46	//Macro eNB transmissino power
 #define AP_power 23		//AP transmission power
 
+#define pktsize 500		//Packet size (bit)
+#define subcarrier 12	//number of subcarrier (12)
+#define	total_symbol 14	//number of symbol in a RB (14)
+#define ctrl_symbol 2	//number of symbol for control signal in a RB (1~3)
+#define resource_element (total_symbol-ctrl_symbol)*subcarrier	//number of resource element per RB for data
+#define total_RBG 100
+
 #include<vector>
 
 enum device_type{macro, ap, ue};
-
+extern struct BS;
+extern struct UE;
 
 /*attribute of BS*/
 struct BS
 {
+	int BSnum;
 	double coor_X, coor_Y;
+	double lambda;
 	double systemT;
+	double capacity;
+	std::vector <UE*> connectingUE;
 	device_type type;
 };
 
 /*attribute of UE*/
 struct UE
 {
+	int UEnum;
 	double coor_X, coor_Y;
 	std::vector <int> CQI_to_neiborBS;
 	std::vector <BS*> neiborBS;
-	double Capacity_to_BS;
-
-	int connecting_BS;
+	std::vector <double> C_to_BS;			//capacity C*
+	std::vector <double> T_to_BS;			//system time T*
+	double Xij;
+	BS* connecting_BS;
 
 	int delaybg;
+	double lambdai;
 };
 
 
 
 void distribution(device_type dtype);
-double getSINR(UE ue, BS bs);
 
 
 
--- dsoffload/sim.cpp
+++ dsoffload/sim.cpp
@@ -14,7 +14,8 @@ int macro_cover[] = { 1732, 1511, 1325, 1162, 1019, 894, 784, 688, 623, 565, 512
 int ap_cover[] = { 82, 68, 60, 50, 39, 30, 28, 26 };
 double macro_SINR[] = { -7, -5, -3, -1, 1, 3, 5, 7, 8.5, 10, 11.5, 13.5, 15, 17, 19.5 };
 int ap_SINR[] = { 22, 21, 20, 16, 12, 9, 7, 4 };
-double eff[15] = { 0.1523, 0.2344, 0.377, 0.6016, 0.8770, 1.1758, 1.4766, 1.9141, 2.4063, 2.7305, 3.3223, 3.9023, 4.5234, 5.1152, 5.5546 };
+double lte_eff[15] = { 0.1523, 0.2344, 0.377, 0.6016, 0.8770, 1.1758, 1.4766, 1.9141, 2.4063, 2.7305, 3.3223, 3.9023, 4.5234, 5.1152, 5.5546 };
+double wifi_capacity[8] = { 6500, 13000, 19500, 26000,	39000, 52000, 58500, 65000 };
 
 /*環境初始設定*/
 void initialconfig()
@@ -24,6 +25,7 @@ void initialconfig()
 	macro.coor_X = 0;
 	macro.coor_Y = 0;
 	macro.type = device_type::macro;
+	macro.BSnum = 0;
 	vbslist.push_back(macro);
 }
 
@@ -50,6 +52,7 @@ void readUE()
 				UE temp;
 				temp.coor_X = stof(bufferx);//String to double
 				temp.coor_Y = stof(buffery);//把座標給UE
+				temp.lambdai = 3;
 				vuelist.push_back(temp);
 			}
 		}
@@ -70,6 +73,7 @@ void readAP()
 	}
 	else
 	{
+		int i = 1;
 		while (!freadAP.eof())
 		{
 			char bufferx[10], buffery[10];
@@ -81,6 +85,7 @@ void readAP()
 				temp.coor_X = stof(bufferx);//String to double
 				temp.coor_Y = stof(buffery);//把座標給AP
 				temp.type = ap;				//設定基地台類型(AP)
+				temp.BSnum = i++;
 				vbslist.push_back(temp);
 			}
 		}
@@ -89,12 +94,11 @@ void readAP()
 }
 
 /*計算UE與BS的CQI*/
-int getCQI(UE u, BS b)
+int getCQI(UE* u, BS* b)
 {
 	int CQI = 0 ;
-	double distance = sqrt(pow((b.coor_X - u.coor_X), 2) + pow((b.coor_Y - u.coor_Y), 2));	//計算距離
-	if (b.type == macro)	//計算LTE的CQI
-	{
+	double distance = sqrt(pow((b->coor_X - u->coor_X), 2) + pow((b->coor_Y - u->coor_Y), 2));	//計算距離
+	if (b->type == macro)	//計算LTE的CQI
 		for (int i = 0; i < 15; i++)
 		{
 			if (distance < macro_cover[i])
@@ -102,9 +106,7 @@ int getCQI(UE u, BS b)
 			else
 				return CQI;
 		}
-	}
-	if (b.type == ap)		//計算Wifi的CQI
-	{
+	if (b->type == ap)		//計算Wifi的CQI
 		for (int i = 0; i < 8; i++)
 		{
 			if (distance < ap_cover[i])
@@ -112,7 +114,6 @@ int getCQI(UE u, BS b)
 			else
 				return CQI;
 		}
-	}
 	return CQI;				//為了消除warning不得不加
 }
 
@@ -126,7 +127,7 @@ void updata_CQI()
 		vuelist[i].neiborBS.clear();
 		for (int j = 0; j < vbslist.size(); j++)
 		{
-			int CQI = getCQI(vuelist[i], vbslist[j]);		//計算UE與BS的CQI
+			int CQI = getCQI(&vuelist[i], &vbslist[j]);		//計算UE與BS的CQI
 			if (CQI != 0)									//如果UE在BS的範圍內
 			{
 				vuelist[i].CQI_to_neiborBS.push_back(CQI);	//紀錄CQI
@@ -135,12 +136,113 @@ void updata_CQI()
 		}
 	}
 		
-	//Show # of neibor BS
-/*	for (int i = 0; i < vuelist.size(); i++)
+/*	//Show # of neibor BS
+	for (int i = 0; i < vuelist.size(); i++)
 		cout << vuelist[i].neiborBS.size() << ", ";
 */
 }
 
+double getCwifi(int CQI)
+{
+	return wifi_capacity[CQI];
+}
+
+double getClte(int CQI)
+{
+	return resource_element * lte_eff[CQI] * total_RBG;
+}
+
+double getC(UE* u, BS* b)
+{
+	int CQI = getCQI(u, b);
+	if (b->type == macro)
+		return getClte(CQI);
+	if (b->type == ap)
+		return getCwifi(CQI);
+	return 9487;
+}
+
+double getT(UE* u, BS* b, int CQI)
+{
+	//試算新加入UE u後的lambda
+	double lambda = 0;
+	for (int i = 0; i < b->connectingUE.size(); i++)
+		lambda += b->connectingUE[i]->lambdai;
+	lambda += u->lambdai;
+	
+	//1. 試算UE u加入後可得的capacity
+	double capa_u;		
+	if (b->type == macro)
+		capa_u = getClte(CQI) / (b->connectingUE.size() + 1);
+	if (b->type == ap)
+		capa_u = getCwifi(CQI) / (b->connectingUE.size() + 1);
+
+	//2. 試算UE u加入後的Xj(avg. service time)
+	double Xj = pktsize / capa_u * (u->lambdai / lambda);		//UE u的Xij(service time)
+	for (int i = 0; i< b->connectingUE.size(); i++)				//加上原本在BS的UE的Xij
+	{
+		//Xj += Xij * (lambdai/lambda)
+		//    = (pktsize /capacity) * (lambdai/lambda)
+		if (b->type == macro)
+			Xj += pktsize / (getClte(getCQI(b->connectingUE[i], b)) / b->connectingUE.size()) * (b->connectingUE[i]->lambdai/ lambda);
+		if (b->type == ap)
+			Xj += pktsize / (getCwifi(getCQI(b->connectingUE[i], b)) / b->connectingUE.size()) * (b->connectingUE[i]->lambdai / lambda);
+	}
+	
+	//3. 試算UE u加入後的Xj2
+	double Xj2 = pow(pktsize / capa_u, 2) * (u->lambdai / lambda);
+	for (int i = 0; i < b->connectingUE.size(); i++)				//加上原本在BS的UE的Xij2
+	{
+		//Xj2 = Xij2 * (lambdai/lambda)
+		//    = (paksize / capacity)^2 * (lambdai/lambda)
+		if (b->type == macro)
+			Xj += (pktsize / (getClte(getCQI(b->connectingUE[i], b)) / b->connectingUE.size()), 2) * (b->connectingUE[i]->lambdai / lambda);
+		if (b->type == ap)
+			Xj += (pktsize / (getCwifi(getCQI(b->connectingUE[i], b)) / b->connectingUE.size()), 2) * (b->connectingUE[i]->lambdai / lambda);
+	}
+
+	//4. 試算UE u加入後的T
+	double T = Xj + (lambda * Xj2) / (1 - (lambda / Xj));
+	return T;
+}
+
+/*尋找合適的BS連接*/
+BS* findBS(UE *u)
+{
+	vector <BS*> neiborBS;
+	BS* candidateBS = &vbslist[0];
+	double minT = 999;
+	//尋找UE的neiborBS
+	for (int i = 0; i < vbslist.size(); i++)
+	{
+		int CQI = getCQI(u, &vbslist[i]);
+		if (CQI != 0)
+			neiborBS.push_back(&vbslist[i]);
+	}
+	//從neiborBS中找適合的BS
+	for (int i = 0; i < u->neiborBS.size(); i++)
+	{
+		int CQI = getCQI(u, u->neiborBS[i]);
+		double T = getT(u, u->neiborBS[i], CQI);
+		if (T < minT)
+		{
+			candidateBS = neiborBS[i];
+			minT = T;
+		}
+		else if (T == minT)
+		{
+			double CapaA, CapaB;
+			CapaA = getC(u, candidateBS);
+			CapaB = getC(u, u->neiborBS[i]);
+			if (CapaB > CapaA)
+			{
+				candidateBS = u->neiborBS[i];
+				minT = T;
+			}
+		}
+	}
+	return candidateBS;
+}
 
 int main()
 {
@@ -158,5 +260,16 @@ int main()
 	for (int i = 0; i < vbslist.size(); i++)
 		cout << "BS " << i << ": X=" << vbslist[i].coor_X << ", Y=" << vbslist[i].coor_Y << "; type=" << vbslist[i].type << "\n";
 */
+	updata_CQI();
+	//UEs associate
+	for (int i = 0; i < vuelist.size(); i++)
+	{
+		vuelist[i].connecting_BS = findBS(&vuelist[i]);
+		vuelist[i].connecting_BS->connectingUE.push_back(&vuelist[i]);
+	}
+
+	//debug
+	for (int i = 0; i < vbslist.size(); i++)
+		cout << "BS" << i << " has " << vbslist[i].connectingUE.size() << "UE, T is " << vbslist[i].systemT << endl;
 	return 0;
 }
